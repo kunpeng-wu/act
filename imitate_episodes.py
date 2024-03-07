774c46772ca461c5b7237375497d91c587e85742
kavinwkp <1817136760@qq.com>
2024-03-07 10:59:25 +0800

    add plot_loss

--- imitate_episodes.py
+++ imitate_episodes.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import pickle
 import argparse
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from copy import deepcopy
 from tqdm import tqdm
 from einops import rearrange
@@ -13,7 +13,7 @@ from constants import PUPPET_GRIPPER_JOINT_OPEN
 from utils import load_data # data functions
 from utils import sample_box_pose, sample_insertion_pose # robot functions
 from utils import compute_dict_mean, set_seed, detach_dict # helper functions
-from policy import ACTPolicy, CNNMLPPolicy
+from policy import ACTPolicy
 from visualize_episodes import save_videos
 
 from sim_env import BOX_POSE
@@ -44,33 +44,33 @@ def main(args):
     dataset_dir = task_config['dataset_dir']
     num_episodes = task_config['num_episodes']
     episode_len = task_config['episode_len']
-    camera_names = task_config['camera_names']
+    camera_names = task_config['camera_names']  # 'top'
 
     # fixed parameters
     state_dim = 14
     lr_backbone = 1e-5
     backbone = 'resnet18'
-    if policy_class == 'ACT':
-        enc_layers = 4
-        dec_layers = 7
-        nheads = 8
-        policy_config = {'lr': args['lr'],
-                         'num_queries': args['chunk_size'],
-                         'kl_weight': args['kl_weight'],
-                         'hidden_dim': args['hidden_dim'],
-                         'dim_feedforward': args['dim_feedforward'],
-                         'lr_backbone': lr_backbone,
-                         'backbone': backbone,
-                         'enc_layers': enc_layers,
-                         'dec_layers': dec_layers,
-                         'nheads': nheads,
-                         'camera_names': camera_names,
-                         }
-    elif policy_class == 'CNNMLP':
-        policy_config = {'lr': args['lr'], 'lr_backbone': lr_backbone, 'backbone' : backbone, 'num_queries': 1,
-                         'camera_names': camera_names,}
-    else:
-        raise NotImplementedError
+    # if policy_class == 'ACT':
+    enc_layers = 4
+    dec_layers = 7
+    nheads = 8
+    policy_config = {'lr': args['lr'],
+                     'num_queries': args['chunk_size'],
+                     'kl_weight': args['kl_weight'],
+                     'hidden_dim': args['hidden_dim'],
+                     'dim_feedforward': args['dim_feedforward'],
+                     'lr_backbone': lr_backbone,
+                     'backbone': backbone,
+                     'enc_layers': enc_layers,
+                     'dec_layers': dec_layers,
+                     'nheads': nheads,
+                     'camera_names': camera_names,
+                     }
+    # elif policy_class == 'CNNMLP':
+    #     policy_config = {'lr': args['lr'], 'lr_backbone': lr_backbone, 'backbone' : backbone, 'num_queries': 1,
+    #                      'camera_names': camera_names,}
+    # else:
+    #     raise NotImplementedError
 
     config = {
         'num_epochs': num_epochs,
@@ -88,11 +88,13 @@ def main(args):
         'real_robot': not is_sim
     }
 
+    # TODO: eval
     if is_eval:
-        ckpt_names = [f'policy_best.ckpt']
+        # ckpt_names = [f'policy_best.ckpt']
+        ckpt_names = [f'policy_epoch_984_seed_0.ckpt']
         results = []
         for ckpt_name in ckpt_names:
-            success_rate, avg_return = eval_bc(config, ckpt_name, save_episode=False)
+            success_rate, avg_return = eval_bc(config, ckpt_name, num_rollouts=1, save_episode=True)
             results.append([ckpt_name, success_rate, avg_return])
 
         for ckpt_name, success_rate, avg_return in results:
@@ -103,11 +105,11 @@ def main(args):
     train_dataloader, val_dataloader, stats, _ = load_data(dataset_dir, num_episodes, camera_names, batch_size_train, batch_size_val)
 
     # save dataset stats
-    if not os.path.isdir(ckpt_dir):
-        os.makedirs(ckpt_dir)
-    stats_path = os.path.join(ckpt_dir, f'dataset_stats.pkl')
-    with open(stats_path, 'wb') as f:
-        pickle.dump(stats, f)
+    # if not os.path.isdir(ckpt_dir):
+    #     os.makedirs(ckpt_dir)
+    # stats_path = os.path.join(ckpt_dir, f'dataset_stats.pkl')
+    # with open(stats_path, 'wb') as f:
+    #     pickle.dump(stats, f)
 
     best_ckpt_info = train_bc(train_dataloader, val_dataloader, config)
     # best_epoch, min_val_loss, best_state_dict = best_ckpt_info
@@ -118,24 +120,24 @@ def main(args):
     # print(f'Best ckpt, val loss {min_val_loss:.6f} @ epoch{best_epoch}')
 
 
-def make_policy(policy_class, policy_config):
-    if policy_class == 'ACT':
-        policy = ACTPolicy(policy_config)
-    elif policy_class == 'CNNMLP':
-        policy = CNNMLPPolicy(policy_config)
-    else:
-        raise NotImplementedError
-    return policy
+# def make_policy(policy_class, policy_config):
+#     if policy_class == 'ACT':
+#         policy = ACTPolicy(policy_config)
+#     elif policy_class == 'CNNMLP':
+#         policy = CNNMLPPolicy(policy_config)
+#     else:
+#         raise NotImplementedError
+#     return policy
 
 
-def make_optimizer(policy_class, policy):
-    if policy_class == 'ACT':
-        optimizer = policy.configure_optimizers()
-    elif policy_class == 'CNNMLP':
-        optimizer = policy.configure_optimizers()
-    else:
-        raise NotImplementedError
-    return optimizer
+# def make_optimizer(policy_class, policy):
+#     if policy_class == 'ACT':
+#         optimizer = policy.configure_optimizers()
+#     elif policy_class == 'CNNMLP':
+#         optimizer = policy.configure_optimizers()
+#     else:
+#         raise NotImplementedError
+#     return optimizer
 
 
 def get_image(ts, camera_names):
@@ -148,12 +150,12 @@ def get_image(ts, camera_names):
     return curr_image
 
 
-def eval_bc(config, ckpt_name, save_episode=False):
+def eval_bc(config, ckpt_name, num_rollouts, save_episode=False):
     set_seed(1000)
     ckpt_dir = config['ckpt_dir']
     state_dim = config['state_dim']
-    real_robot = config['real_robot']
-    policy_class = config['policy_class']
+    # real_robot = config['real_robot']
+    # policy_class = config['policy_class']
     onscreen_render = config['onscreen_render']
     policy_config = config['policy_config']
     camera_names = config['camera_names']
@@ -164,7 +166,8 @@ def eval_bc(config, ckpt_name, save_episode=False):
 
     # load policy and stats
     ckpt_path = os.path.join(ckpt_dir, ckpt_name)
-    policy = make_policy(policy_class, policy_config)
+    # policy = make_policy(policy_class, policy_config)
+    policy = ACTPolicy(policy_config)
     loading_status = policy.load_state_dict(torch.load(ckpt_path))
     print(loading_status)
     policy.cuda()
@@ -189,13 +192,13 @@ def eval_bc(config, ckpt_name, save_episode=False):
     env_max_reward = env.task.max_reward
 
     query_frequency = policy_config['num_queries']
+    num_queries = 0
     if temporal_agg:
         query_frequency = 1
         num_queries = policy_config['num_queries']
 
     max_timesteps = int(max_timesteps * 1) # may increase for real-world tasks
 
-    num_rollouts = 1
     episode_returns = []
     highest_rewards = []
     for rollout_id in range(num_rollouts):
@@ -208,13 +211,15 @@ def eval_bc(config, ckpt_name, save_episode=False):
 
         ts = env.reset()
 
-        ### onscreen render
-        # if onscreen_render:
-        #     ax = plt.subplot()
-        #     plt_img = ax.imshow(env._physics.render(height=480, width=640, camera_id=onscreen_cam))
-        #     plt.ion()
+        # onscreen render
+        plt_img = None
+        if onscreen_render:
+            ax = plt.subplot()
+            plt_img = ax.imshow(env._physics.render(height=480, width=640, camera_id=onscreen_cam))
+            plt.ion()
 
-        ### evaluation loop
+        # evaluation loop
+        all_time_actions = None
         if temporal_agg:
             all_time_actions = torch.zeros([max_timesteps, max_timesteps+num_queries, state_dim]).cuda()
 
@@ -225,11 +230,11 @@ def eval_bc(config, ckpt_name, save_episode=False):
         rewards = []
         with torch.inference_mode():
             for t in range(max_timesteps):
-                ### update onscreen render and wait for DT
-                # if onscreen_render:
-                #     image = env._physics.render(height=480, width=640, camera_id=onscreen_cam)
-                #     plt_img.set_data(image)
-                #     plt.pause(DT)
+                # update onscreen render and wait for DT
+                if onscreen_render:
+                    image = env._physics.render(height=480, width=640, camera_id=onscreen_cam)
+                    plt_img.set_data(image)
+                    plt.pause(DT)
 
                 ### process previous timestep to get qpos and image_list
                 obs = ts.observation
@@ -250,7 +255,7 @@ def eval_bc(config, ckpt_name, save_episode=False):
                     if temporal_agg:
                         all_time_actions[[t], t:t+num_queries] = all_actions
                         actions_for_curr_step = all_time_actions[:, t]
-                        actions_populated = torch.all(actions_for_curr_step != 0, axis=1)
+                        actions_populated = torch.all(actions_for_curr_step != 0, dim=1)
                         actions_for_curr_step = actions_for_curr_step[actions_populated]
                         k = 0.01
                         exp_weights = np.exp(-k * np.arange(len(actions_for_curr_step)))
@@ -259,25 +264,25 @@ def eval_bc(config, ckpt_name, save_episode=False):
                         raw_action = (actions_for_curr_step * exp_weights).sum(dim=0, keepdim=True)
                     else:
                         raw_action = all_actions[:, t % query_frequency]
-                elif config['policy_class'] == "CNNMLP":
-                    raw_action = policy(qpos, curr_image)
-                else:
-                    raise NotImplementedError
+                # elif config['policy_class'] == "CNNMLP":
+                #     raw_action = policy(qpos, curr_image)
+                # else:
+                #     raise NotImplementedError
 
                 ### post-process actions
                 raw_action = raw_action.squeeze(0).cpu().numpy()
                 action = post_process(raw_action)
                 target_qpos = action
 
-                ### step the environment
+                # step the environment
                 ts = env.step(target_qpos)
 
-                ### for visualization
+                # for visualization
                 qpos_list.append(qpos_numpy)
                 target_qpos_list.append(target_qpos)
                 rewards.append(ts.reward)
 
-            # plt.close()
+            plt.close()
         # if real_robot:
         #     move_grippers([env.puppet_bot_left, env.puppet_bot_right], [PUPPET_GRIPPER_JOINT_OPEN] * 2, move_time=0.5)  # open
         #     pass
@@ -328,10 +333,11 @@ def train_bc(train_dataloader, val_dataloader, config):
 
     set_seed(seed)
 
-    policy = make_policy(policy_class, policy_config)
+    # policy = make_policy(policy_class, policy_config)
+    policy = ACTPolicy(policy_config)
     policy.cuda()
-    optimizer = make_optimizer(policy_class, policy)
-
+    # optimizer = make_optimizer(policy_class, policy)
+    optimizer = policy.configure_optimizers()
     train_history = []
     validation_history = []
     min_val_loss = np.inf
@@ -339,19 +345,19 @@ def train_bc(train_dataloader, val_dataloader, config):
     for epoch in range(num_epochs):
         print(f'\nEpoch {epoch}')
         # validation
-        # with torch.inference_mode():
-        #     policy.eval()
-        #     epoch_dicts = []
-        #     for batch_idx, data in enumerate(val_dataloader):
-        #         forward_dict = forward_pass(data, policy)
-        #         epoch_dicts.append(forward_dict)
-        #     epoch_summary = compute_dict_mean(epoch_dicts)
-        #     validation_history.append(epoch_summary)
-        #
-        #     epoch_val_loss = epoch_summary['loss']
-        #     if epoch_val_loss < min_val_loss:
-        #         min_val_loss = epoch_val_loss
-        #         best_ckpt_info = (epoch, min_val_loss, deepcopy(policy.state_dict()))
+        with torch.inference_mode():
+            policy.eval()
+            epoch_dicts = []
+            for batch_idx, data in enumerate(val_dataloader):
+                forward_dict = forward_pass(data, policy)
+                epoch_dicts.append(forward_dict)
+            epoch_summary = compute_dict_mean(epoch_dicts)
+            validation_history.append(epoch_summary)
+
+            epoch_val_loss = epoch_summary['loss']
+            if epoch_val_loss < min_val_loss:
+                min_val_loss = epoch_val_loss
+                best_ckpt_info = (epoch, min_val_loss, deepcopy(policy.state_dict()))
         # print(f'Val loss:   {epoch_val_loss:.5f}')
         # summary_string = ''
         # for k, v in epoch_summary.items():
@@ -364,7 +370,7 @@ def train_bc(train_dataloader, val_dataloader, config):
         batch_idx = 0
         for batch_idx, data in enumerate(train_dataloader):
             # forward_dict = forward_pass(data, policy)
-            image_data, qpos_data, action_data, is_pad = data
+            image_data, qpos_data, action_data, is_pad = data   # (8,1,3,480,640) (8,14) (8,400,14) (8,400)
             image_data, qpos_data, action_data, is_pad = image_data.cuda(), qpos_data.cuda(), action_data.cuda(), is_pad.cuda()
             forward_dict = policy(qpos_data, image_data, action_data, is_pad)   # {'kl','l1','loss'}
             # backward
@@ -376,10 +382,10 @@ def train_bc(train_dataloader, val_dataloader, config):
         epoch_summary = compute_dict_mean(train_history[(batch_idx+1)*epoch:(batch_idx+1)*(epoch+1)])
         epoch_train_loss = epoch_summary['loss']
         print(f'Train loss: {epoch_train_loss:.5f}')
-        summary_string = ''
-        for k, v in epoch_summary.items():
-            summary_string += f'{k}: {v.item():.3f} '
-        print(summary_string)
+        # summary_string = ''
+        # for k, v in epoch_summary.items():
+        #     summary_string += f'{k}: {v.item():.3f} '
+        # print(summary_string)
 
         # if epoch % 100 == 0:
         #     ckpt_path = os.path.join(ckpt_dir, f'policy_epoch_{epoch}_seed_{seed}.ckpt')
@@ -388,33 +394,55 @@ def train_bc(train_dataloader, val_dataloader, config):
 
     # ckpt_path = os.path.join(ckpt_dir, f'policy_last.ckpt')
     # torch.save(policy.state_dict(), ckpt_path)
-
+    # TODO: save model parameter
     # best_epoch, min_val_loss, best_state_dict = best_ckpt_info
     # ckpt_path = os.path.join(ckpt_dir, f'policy_epoch_{best_epoch}_seed_{seed}.ckpt')
     # torch.save(best_state_dict, ckpt_path)
     # print(f'Training finished:\nSeed {seed}, val loss {min_val_loss:.6f} at epoch {best_epoch}')
 
-    # save training curves
-    # plot_history(train_history, validation_history, num_epochs, ckpt_dir, seed)
+    # TODO: save training curves
+    plot_history(train_history, validation_history, num_epochs, ckpt_dir, seed)
 
     return best_ckpt_info
 
 
-# def plot_history(train_history, validation_history, num_epochs, ckpt_dir, seed):
-#     # save training curves
-#     for key in train_history[0]:
-#         plot_path = os.path.join(ckpt_dir, f'train_val_{key}_seed_{seed}.png')
-#         plt.figure()
-#         train_values = [summary[key].item() for summary in train_history]
-#         val_values = [summary[key].item() for summary in validation_history]
-#         plt.plot(np.linspace(0, num_epochs-1, len(train_history)), train_values, label='train')
-#         plt.plot(np.linspace(0, num_epochs-1, len(validation_history)), val_values, label='validation')
-#         # plt.ylim([-0.1, 1])
-#         plt.tight_layout()
-#         plt.legend()
-#         plt.title(key)
-#         plt.savefig(plot_path)
-#     print(f'Saved plots to {ckpt_dir}')
+def plot_history(train_history, validation_history, num_epochs, ckpt_dir, seed):
+    # save training curves
+    # for key in train_history[0]:
+    for key in {'loss'}:
+        plot_path = os.path.join(ckpt_dir, f'train_val_{key}_seed_{seed}.png')
+        plt.figure()
+        train_values = [summary[key].item() for summary in train_history]
+        eval_values = [summary[key].item() for summary in validation_history]
+        np.save('checkpoints/train_loss.npy', train_values)
+        np.save('checkpoints/eval_loss.npy', eval_values)
+        plt.plot(np.linspace(0, num_epochs-1, len(train_history)), train_values, label='train')
+        plt.plot(np.linspace(0, num_epochs-1, len(validation_history)), eval_values, label='validation')
+        # plt.ylim([-0.1, 1])
+        plt.tight_layout()
+        plt.legend()
+        plt.title(key)
+        # plt.savefig(plot_path)
+    print(f'Saved plots to {ckpt_dir}')
+    plt.show()
+
+
+def plot_loss():
+    num_epochs = 10
+    train_loss = np.load('checkpoints/train_loss.npy')
+    eval_loss = np.load('checkpoints/eval_loss.npy')
+    print(train_loss)
+    print(train_loss.shape)
+    print(eval_loss)
+    print(eval_loss.shape)
+    plt.figure()
+    plt.plot(np.linspace(0, num_epochs - 1, len(train_loss)), train_loss, label='train')
+    plt.plot(np.linspace(0, num_epochs - 1, len(eval_loss)), eval_loss, label='validation')
+    # plt.ylim([-0.1, 1])
+    plt.tight_layout()
+    plt.legend()
+    plt.title('loss')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -425,8 +453,8 @@ if __name__ == '__main__':
     parser.add_argument('--policy_class', type=str, help='policy_class, capitalize', default="ACT")
     parser.add_argument('--task_name', type=str, help='task_name', default="sim_transfer_cube_scripted")
     parser.add_argument('--batch_size', type=int, help='batch_size', default=8)
-    parser.add_argument('--seed', type=int, help='seed', default=0)
-    parser.add_argument('--num_epochs', type=int, help='num_epochs', default=3)
+    parser.add_argument('--seed', type=int, help='seed', default=1)
+    parser.add_argument('--num_epochs', type=int, help='num_epochs', default=10)
     parser.add_argument('--lr', type=float, help='lr', default=1e-5)
 
     # for ACT
@@ -435,5 +463,6 @@ if __name__ == '__main__':
     parser.add_argument('--hidden_dim', action='store', type=int, help='hidden_dim', default=512)
     parser.add_argument('--dim_feedforward', action='store', type=int, help='dim_feedforward', default=3200)
     parser.add_argument('--temporal_agg', action='store_true')
-    
+
     main(vars(parser.parse_args()))
+    # plot_loss()
